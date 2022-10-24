409289058500f977129d62701c63b9fbaec824d1
karigo <karigo333@gmail.com>
2022-10-25 00:30:40 +0300

    say Hello

--- index.php
+++ index.php
@@ -7,6 +7,13 @@ $update = json_decode($input);
 $message = $update->message;
 $chat_id = $message->chat->id;
 $token = "5408077710:AAEKDl9B75vkmPYVYHxET5aW03OI6-czrJQ";
-$user_id = $message->chat->first_name;
+$username = $message->chat->first_name;
+$check_negative_id = gmp_sign($chat_id);
 
-file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет " . $user_id . "! Уже с сервака прислал тебе ответ");
+if($message->text == '/start' && $check_negative_id == 1) {
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет " . $username . "!");
+}
+
+if($message->text == '/start' && $check_negative_id == -1) {
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Приветствую " . $username . "!");
+}
