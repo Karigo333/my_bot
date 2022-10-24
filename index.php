372db42380ef6b9d62383ba35ccef61e7535fcee
karigo <karigo333@gmail.com>
2022-10-25 02:30:15 +0300

    add db fix

--- index.php
+++ index.php
@@ -23,12 +23,12 @@ $connection = pg_connect("host=$host dbname=$database user=$user password=$passw
 
 if($message->text == '/start' && $chat_id < 0) {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Приветcтвую  " . $username_chat . "!");
-    $query = "INSERT INTO users (id, name,message, is_private) VALUES ($chat_id, $username_chat, '$message', false);";
+    $query = "INSERT INTO users (id, name,message) VALUES ($chat_id, $username_chat, '$message');";
     pg_query($connection, $query);
 }
 if($message->text == '/start' && $chat_id > 0) {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет  " . $username . "!");
-    $query = "INSERT INTO users (id, name, message, is_private) VALUES ($chat_id, $username, '$message', true);";
+    $query = "INSERT INTO users (id, name, message) VALUES ($chat_id, $username, '$message');";
     pg_query($connection, $query);
 }
 
