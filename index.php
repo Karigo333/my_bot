a31d3ca0874b64bb376ec0421b1dd8e75c474f6d
karigo <karigo333@gmail.com>
2022-10-25 00:46:57 +0300

    say Hello chat

--- index.php
+++ index.php
@@ -8,8 +8,13 @@ $message = $update->message;
 $chat_id = $message->chat->id;
 $token = "5408077710:AAEKDl9B75vkmPYVYHxET5aW03OI6-czrJQ";
 $username = $message->chat->first_name;
+$username_chat = $message->from->first_name;
+$check_negative_id = gmp_sign($chat_id);
 
-if($message->text == '/start') {
+if($message->text == '/start' && $check_negative_id == 1) {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет " . $username . "!");
 }
 
+if($message->text == '/start' && $check_negative_id == -1) {
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Приветствую " . $username_chat . "!");
+}
