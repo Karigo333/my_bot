47dde965c5c1faf41554c0b2d18cff48c9ae7bad
karigo <karigo333@gmail.com>
2022-10-25 01:46:45 +0300

    add db

--- index.php
+++ index.php
@@ -10,6 +10,20 @@ $token = "5408077710:AAEKDl9B75vkmPYVYHxET5aW03OI6-czrJQ";
 $username = $message->chat->first_name;
 $username_chat = $message->from->first_name;
 
+$db_uri = 'postgres://sxmwymqjhierix:63cb61a19f61f365516b3ff7bd68b7d5092ac9e49e9786ed6eb30483d13696b8@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d7g25fppntp8qk';
+
+$host = "ec2-54-228-218-84.eu-west-1.compute.amazonaws.com";
+$database = "d7g25fppntp8qk";
+$user = "sxmwymqjhierix";
+$password = "63cb61a19f61f365516b3ff7bd68b7d5092ac9e49e9786ed6eb30483d13696b8";
+
+$connection = pg_connect("host=$host dbname=$database user=$user password=$password");
+
+$query = "DROP TABLE IF EXISTS inventory;";
+pg_query($connection, $query);
+
+$query = "CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);";
+pg_query($connection, $query);
 
 if($message->text == '/start' && $chat_id < 0) {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Приветcтвую  " . $username_chat . "!");
