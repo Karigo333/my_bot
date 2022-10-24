3127f12ef0a792a8b3d1958522fa596c04c9847e
karigo <karigo333@gmail.com>
2022-10-24 23:44:08 +0300

    user id

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ $update = json_decode($input);
 $message = $update->message;
 $chat_id = $message->chat->id;
 $token = "5408077710:AAEKDl9B75vkmPYVYHxET5aW03OI6-czrJQ";
+$user_id = $message->user->id;
 
-file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Hello");
+file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет " . $user_id . "!");
