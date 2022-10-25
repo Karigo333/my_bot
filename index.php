cfbe576cb703ab7ffa16df7f560ce9feacc95905
karigo <karigo333@gmail.com>
2022-10-25 03:20:31 +0300

    add db fix

--- index.php
+++ index.php
@@ -20,22 +20,23 @@ $password = "63cb61a19f61f365516b3ff7bd68b7d5092ac9e49e9786ed6eb30483d13696b8";
 
 $connection = pg_connect("host=$host dbname=$database user=$user password=$password");
 
-$query = "DROP TABLE IF EXISTS inv;";
+$query = "DROP TABLE IF EXISTS tast;";
 pg_query($connection, $query);
 
-$query = "CREATE TABLE test (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);";
+$query = "CREATE TABLE trtre (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);";
 pg_query($connection, $query);
 
+file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет  " . $username . "!");
 
 if($message->text == '/start' && $chat_id < 0) {
-//    $query = "INSERT INTO users (id, name, message) VALUES ($chat_id, $username_chat, '$message');";
-//    pg_query($connection, $query);
+    $query = "INSERT INTO users (id, name, message) VALUES ($chat_id, $username_chat, '$message');";
+    pg_query($connection, $query);
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Приветcтвую  " . $username_chat . "!");
 
 }
 if($message->text == '/start' && $chat_id > 0) {
-//    $query = "INSERT INTO users (id, name, message) VALUES ($chat_id, $username, '$message');";
-//    pg_query($connection, $query);
+    $query = "INSERT INTO users (id, name, message) VALUES ($chat_id, $username, '$message');";
+    pg_query($connection, $query);
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chat_id . "&text=Привет  " . $username . "!");
 
 }
